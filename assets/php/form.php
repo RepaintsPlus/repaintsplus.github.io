843ba828cf78add83834c90bed947840293fa865
AnAussiePilot <FlynnStarr@hotmail.com>
2021-10-30 17:40:54 +1100

    email

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,6 +1,17 @@
-<?php
-if($_POST["Message"]) {
-mail("repaintsplus@outlook.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: jane@janedoe.com");
-}
+<?php 
+if(isset($_POST['submit'])){
+    $to = "repaintsplus@outlook.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $full_name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    }
 ?>
